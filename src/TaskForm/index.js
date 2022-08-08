99d6246228b1b17fa676bb52a2d7a0679be8537d
namDinhSong <namdinh1993@gmail.com>
2022-08-08 18:20:35 +0700

    update hook

--- src/TaskForm/index.js
+++ src/TaskForm/index.js
@@ -1,42 +1,70 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router";
 import "./style.scss";
 
 function TaskForm(props) {
+  const navigate = useNavigate();
   const [formValue, setFormValue] = useState({
-    title:'',
-    author:'',
-    description:''
-  })
+    title: "",
+    author: "",
+    description: "",
+    status: ""
+  });
   const handleFormValue = (e) => {
     setFormValue({
       ...formValue,
-      [e.target.name]:e.target.value,
-    })
-    console.log(e);
-  }
+      [e.target.name]: e.target.value,
+    });
+    // console.log(e);
+  };
+  const handleChangeForm = (e) => {
+    // no reload
+    e.preventDefault();
+    // validate
+    if (
+      formValue.title !== "" &&
+      formValue.author !== "" &&
+      formValue.description !== ""
+    ) {
+      const data = JSON.parse(localStorage.getItem("data")) || [];
+      const has_item =
+        data.find((item) => item.title === formValue.title) != null;
+      if (has_item) {
+        alert("Duplicated");
+      } else {
+        data.push(formValue);
+        // local storage
+        localStorage.setItem("data", JSON.stringify(data));
+      }
+    } else {
+      alert("Nhap lai");
+    };
+    // naviga to home
+    navigate("../home");
+  };
   return (
     <div className="form-contain">
-      <form onSubmit={handleFormValue}>
+      <form onSubmit={handleChangeForm}>
         <h2>Create New Task</h2>
-        <input 
-        placeholder="Title..." 
-        name="title"
-        value={formValue.title}
-        onChange={handleFormValue}
+        <input
+          placeholder="Title..."
+          name="title"
+          value={formValue.title}
+          onChange={handleFormValue}
         />
-        <input 
-        placeholder="Creator..." 
-        name="author"
-        value={formValue.author}
-        onChange={handleFormValue}
+        <input
+          placeholder="Creator..."
+          name="author"
+          value={formValue.author}
+          onChange={handleFormValue}
         />
-        <input 
-        placeholder="Description..." 
-        name="description"
-        value={formValue.description}
-        onChange={handleFormValue}
+        <input
+          placeholder="Description..."
+          name="description"
+          value={formValue.description}
+          onChange={handleFormValue}
         />
-        <button/>
+        <button type="submit" onClick={handleChangeForm}>Add</button>
       </form>
     </div>
   );
--- src/components/constant/data.jsx
+++ src/components/constant/data.jsx
@@ -1,40 +1,40 @@
 export const data = [
-  {
-    title: "Task 1",
-    creator: "Nam",
-    status: "New",
-    description:
-      "This is a task. This is a task. This is a task. This is a task. This is a task. This is a task. ",
-  },
-  {
-    title: "Task 1",
-    creator: "Nam",
-    status: "New",
-    description:
-      "This is a task. This is a task. This is a task. This is a task. This is a task. This is a task. ",
-  },
+  // {
+  //   title: "Task 1",
+  //   creator: "Nam",
+  //   status: "New",
+  //   description:
+  //     "This is a task. This is a task. This is a task. This is a task. This is a task. This is a task. ",
+  // },
+  // {
+  //   title: "Task 1",
+  //   creator: "Nam",
+  //   status: "New",
+  //   description:
+  //     "This is a task. This is a task. This is a task. This is a task. This is a task. This is a task. ",
+  // },
 
-  {
-    title: "Task 1",
-    creator: "Nam",
-    status: "New",
-    description:
-      "This is a task. This is a task. This is a task. This is a task. This is a task. This is a task. ",
-  },
+  // {
+  //   title: "Task 1",
+  //   creator: "Nam",
+  //   status: "New",
+  //   description:
+  //     "This is a task. This is a task. This is a task. This is a task. This is a task. This is a task. ",
+  // },
 
-  {
-    title: "Task 1",
-    creator: "Nam",
-    status: "New",
-    description:
-      "This is a task. This is a task. This is a task. This is a task. This is a task. This is a task. ",
-  },
+  // {
+  //   title: "Task 1",
+  //   creator: "Nam",
+  //   status: "New",
+  //   description:
+  //     "This is a task. This is a task. This is a task. This is a task. This is a task. This is a task. ",
+  // },
 
-  {
-    title: "Task 1",
-    creator: "Nam",
-    status: "New",
-    description:
-      "This is a task. This is a task. This is a task. This is a task. This is a task. This is a task. ",
-  },
+  // {
+  //   title: "Task 1",
+  //   creator: "Nam",
+  //   status: "New",
+  //   description:
+  //     "This is a task. This is a task. This is a task. This is a task. This is a task. This is a task. ",
+  // },
 ];
